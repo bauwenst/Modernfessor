66c83b1698c2b701064dc2e4e1920ae4a57e6189
Lukas Mielczarek <lukas.mielczarek@uni-duesseldorf.de>
2025-04-02 15:33:29 +0200

    Created Morfessor Vocabulariser class

---
+++ src/modernfessor/baseline/vocabulariser.py
@@ -0,0 +1,14 @@
+from typing import Dict, Tuple, Union, List, Iterable
+from pathlib import Path
+from enum import Enum
+from collections import defaultdict, OrderedDict
+
+import json
+from tqdm.auto import tqdm
+
+# Core libraries
+from tktkt.interfaces.vocabulariser import Vocabulariser
+
+
+class MorfessorVocabulariser(Vocabulariser):
+    pass
